00ef7ff09d9fa002cda7f843604f0f5a1adb377b
michael-ji0406 <michael.ji@dji.com>
2016-04-29 17:46:08 +0800

    redefine token generator method and remove set token callback after user save

--- lib/simple_token_authentication/token_generator.rb
+++ lib/simple_token_authentication/token_generator.rb
@@ -4,8 +4,12 @@ module SimpleTokenAuthentication
   class TokenGenerator
     include Singleton
 
-    def generate_token
-      Devise.friendly_token
+		def generate_token
+   		if block_given?
+   			yield
+   		else
+      	Devise.friendly_token
+      end
     end
   end
 end
